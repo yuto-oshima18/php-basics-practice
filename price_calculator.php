2d53dafe184c248fc44ee3d189b96223bd7d3a9a
Yuto Oshima <yuto.oshima814@gmail.com>
2026-07-07 01:02:53 +0900

    update price_calculator.php

--- price_calculator.php
+++ price_calculator.php
@@ -1,5 +1,5 @@
 <?php
-$product_name = 'ノートパソコン';
+$product_name = "ノートパソコン";
 $price = 80000;
 $quantity = 2;
 $tax_rate = 0.1;
@@ -8,9 +8,9 @@ $subtotal = $price * $quantity;
 $tax_amount = $subtotal * $tax_rate;
 $total = $subtotal + $tax_amount;
 
-echo "商品名: $product_name<br>";
-echo '単価: '.$price.'円<br>';
-echo '数量: '.$quantity.'個<br>';
-echo '小計: '.$subtotal.'円<br>';
-echo '消費税(10%): '.$tax_amount.'円<br>';
-echo '合計金額: '.$total.'円<br>';
+echo "商品名: ".$product_name."<br>";
+echo "単価: ".$price."円<br>";
+echo "数量: ".$quantity."個<br>";
+echo "小計: ".$subtotal."円<br>";
+echo "消費税(10%): ".$tax_amount."円<br>";
+echo "<strong>合計金額: ".$total."円</strong><br>";
